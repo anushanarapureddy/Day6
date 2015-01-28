9f7af0d42199297ae7ef2404adbce12a6ad5a9ff
anushanarapureddy <n.anusha108@gmail.com>
2015-01-28 12:10:36 +0530

    two change add

---
+++ settings.js
@@ -0,0 +1,11 @@
+/**
+ * Created by haranath.gorantla on 9/12/2014.
+ */
+module.exports = {
+    details: {
+        ipAddress: "localhost",
+        port : 27017,
+        dbName :"example",
+        enabled: true
+    }
+};
